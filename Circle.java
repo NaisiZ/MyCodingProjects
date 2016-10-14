51b93894653aec01b0c5f37ef21f9abd6e24b6e1
NaisiZ <zhennaisi@gmail.com>
2016-10-13 23:28:25 -0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,37 @@
+package Exercise3;
+
+public class Circle { 
+	private double radius;
+	private String color;
+	public Circle() {
+	this.radius = 1.0;
+	this.color = "red";
+	}
+	public Circle(double r) {
+	this.radius = r;
+	this.color = "red";
+	}
+	
+	public double getRadius() {
+	return radius;
+	}
+	public Circle(double r,String c){
+		this.radius = r;
+		this.color = c;
+	}
+	public String getColor(){
+		return color;
+	}
+	public void setRadius(double r) {
+		this.radius = r;
+		}
+	public String toString() {
+		return "Circle: radius=" + radius + " color="+ color;
+		}
+		public void setColor(String c) {
+			this.color=c;
+		}
+	public double getArea() {
+	return radius*radius*Math.PI;
+	}
+}
---
+++ TestCircle.java
@@ -0,0 +1,26 @@
+package Exercise3;
+
+public class TestCircle {
+
+	public static void main(String[] args) {
+		Circle c1=new Circle();
+		System.out.println(c1.getRadius());
+		System.out.println(c1.getArea());
+		Circle c2=new Circle(2);
+		System.out.println(c2.getRadius());
+		System.out.println(c2.getArea());
+		Circle c3=new Circle(3,"yellow");
+		System.out.println(c3.getRadius());
+		System.out.println(c3.getArea());
+		System.out.println(c3.getColor());
+		Circle c4=new Circle();
+		c4.setRadius(5.0);          
+		c4.setColor("green");       
+		System.out.println(c4.getRadius());
+		System.out.println(c4.getArea());
+		System.out.println(c4.getColor());
+		Circle c5= new Circle(5.0);
+		System.out.println(c5.toString());
+	}
+
+}
